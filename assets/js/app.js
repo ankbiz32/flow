5ade32f83177eef5f1bbc41221d9347bb105e274
= <developer@cluebixsoftware.com>
2021-01-30 18:31:39 +0530

    done

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,3 +5,32 @@ $(function () {
         $nav.toggleClass('scrolled', $(this).scrollTop() > $scrollHeight);
     });
 });
+
+
+// Feedbacks slider
+$('.feedback-container').owlCarousel({
+    nav:true,
+    loop:true,
+    dots:false,
+    autoplay:true,
+    autoplayTimeout:3000,
+    autoplayHoverPause:true,
+    navText: ["<i class='fas fa-caret-left'></i>","<i class='fas fa-caret-right'></i>"],
+    responsive:{
+        0:{
+            margin:15,
+            stagePadding:30,
+            items:1
+        },
+        600:{
+            margin:10,
+            stagePadding:60,
+            items:2
+        },
+        1000:{
+            margin:15,
+            stagePadding:80,
+            items:2
+        }
+    }
+})
